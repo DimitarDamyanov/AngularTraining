7ea6748f7acf7db66686370473e9866b39d5b293
DimitarDamyanov <ddamqnov@gmail.com>
2016-03-21 22:18:54 +0200

    image mongoose model

---
+++ models/image.js
@@ -0,0 +1,13 @@
+/**
+ * Created by D on 21.3.2016 ã..
+ */
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var image = new Schema({
+    name: {type: String},
+    title: {type: String}
+});
+
+var imageModel = mongoose.model('Image', image);
+module.exports = imageModel;
--- models/pc.js
+++ models/pc.js
@@ -3,12 +3,14 @@
  */
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema,
-    Part = require('./part');
+    Part = require('./part'),
+    Image = require('./image');
 
 var pc = new Schema({
     name: {type: String}, /*Should be required field*/
     username: {type: String}, /*Should be required field*/
     parts: [{type: mongoose.Schema.Types.ObjectId, ref: 'Part'}],
+    images: [{type: mongoose.Schema.Types.ObjectId, ref: 'Image'}],
     time: {type: Date, default: Date.now}
 });
 
